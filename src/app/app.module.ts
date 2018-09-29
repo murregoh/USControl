efaf28e3eb2c514ea88a52b63c540a7267bb3582
Mateo Urrego Hoyos <mateo.urregoh@gmail.com>
2018-09-29 16:49:20 -0500

    uploading files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PanelComponent } from './components/shared/panel/panel.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { UsCardComponent } from './components/us-card/us-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    UsCardComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/us-card/us-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-us-card',
+  templateUrl: './us-card.component.html',
+  styleUrls: ['./us-card.component.css']
+})
+export class UsCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
